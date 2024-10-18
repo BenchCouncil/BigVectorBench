4cd77ec5d0e58795fd16a7b77f2067ef3d69ed51
Lewisvan <576321116@qq.com>
2024-10-18 15:30:06 +0800

    ADD PGVECTOR

--- bigvectorbench/algorithms/pgvector/module.py
+++ bigvectorbench/algorithms/pgvector/module.py
@@ -2,9 +2,10 @@
 
 import subprocess
 import sys
-
+import numpy as np
 import pgvector.psycopg
 import psycopg
+import os
 
 from bigvectorbench.algorithms.base.module import BaseANN
 
@@ -14,26 +15,50 @@ class PGVector(BaseANN):
         self._m = method_param['M']
         self._ef_construction = method_param['efConstruction']
         self._cur = None
+        self.labels = None
+        self.label_names = None
+        self.label_types = None
+        
         if metric == "angular":
             self._query = "SELECT id FROM items ORDER BY embedding <=> %s LIMIT %s"
         elif metric == "euclidean":
             self._query = "SELECT id FROM items ORDER BY embedding <-> %s LIMIT %s"
         else:
             raise RuntimeError(f"unknown metric {metric}")
-        
-    def fit(self, X):
+
+    def load_data(
+        self,
+        embeddings: np.array,
+        labels: np.ndarray | None = None,
+        label_names: list[str] | None = None,
+        label_types: list[str] | None = None,
+    ) -> None:
         subprocess.run("service postgresql start", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
         conn = psycopg.connect(user="bvb", password="bvb", dbname="bvb", autocommit=True)
         pgvector.psycopg.register_vector(conn)
         cur = conn.cursor()
         cur.execute("DROP TABLE IF EXISTS items")
-        cur.execute("CREATE TABLE items (id int, embedding vector(%d))" % X.shape[1])
+
+        if labels is not None and label_names and label_types:
+            additional_columns = ', '.join(f"{name} {type}" for name, type in zip(label_names, label_types))
+            table_definition = f"id int, embedding vector({embeddings.shape[1]}), {additional_columns}"
+        else:
+            table_definition = f"id int, embedding vector({embeddings.shape[1]})"
+        
+        cur.execute(f"CREATE TABLE items ({table_definition})")
         cur.execute("ALTER TABLE items ALTER COLUMN embedding SET STORAGE PLAIN")
-        print("copying data...")
-        with cur.copy("COPY items (id, embedding) FROM STDIN WITH (FORMAT BINARY)") as copy:
-            copy.set_types(["int4", "vector"])
-            for i, embedding in enumerate(X):
-                copy.write_row((i, embedding))
+
+        if labels is not None:
+            with cur.copy(f"COPY items (id, embedding, {', '.join(label_names)}) FROM STDIN WITH (FORMAT BINARY)") as copy:
+                copy.set_types(["int4", "vector"] + label_types)
+                for i, embedding in enumerate(embeddings):
+                    copy.write_row((i, embedding) + tuple(labels[i]))
+        else:
+            with cur.copy("COPY items (id, embedding) FROM STDIN WITH (FORMAT BINARY)") as copy:
+                copy.set_types(["int4", "vector"])
+                for i, embedding in enumerate(embeddings):
+                    copy.write_row((i, embedding))
+
         print("creating index...")
         if self._metric == "angular":
             cur.execute("CREATE INDEX ON items USING hnsw (embedding vector_cosine_ops) WITH (m = %d, ef_construction = %d)" % (self._m, self._ef_construction))
@@ -48,7 +73,12 @@ class PGVector(BaseANN):
         self._ef_search = ef_search
         self._cur.execute("SET hnsw.ef_search = %d" % ef_search)
     
-    def query(self, v, n):
+    def query(self, v: np.array, n: int, filter_expr: str | None = None) -> list[int]:
+        if filter_expr:
+            sql_filter = " AND ".join(f"{name} = {value}" for name, value in zip(self.label_names, eval(filter_expr)))
+            query = self._query[:-8] + " AND " + sql_filter + self._query[-8:]
+        else:
+            query = self._query
         self._cur.execute(self._query, (v, n), binary=True, prepare=True)
         return [id for id, in self._cur.fetchall()]
     
